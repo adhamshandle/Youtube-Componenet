3f3245105305a2f7ebd47c476c5f0cc46b71a982
Adham Khaled <adhamkhaled2427@gmail.com>
2018-09-06 16:12:18 +0200

    Update video.js

--- src/components/video.js
+++ src/components/video.js
@@ -4,7 +4,7 @@ import React from 'react';
 const VideoDetail = ({video}) => {
 
 	if (!video) {
-		return <div>Loading...</div>;
+		return <div className='bo'>Loading...</div>;
 	}
 
 	const videoId = video.id.videoId;
